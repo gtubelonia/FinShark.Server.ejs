16beb647f8f94c722e291922ab5879298587a1e1
Gian T <Gtubelonia@live.com>
2024-09-22 12:52:22 -0500

    added a datehelper util

---
+++ src/utils/dateHelper.js
@@ -0,0 +1,11 @@
+exports.GetCurrentDate = function () {
+    const date = new Date()
+
+    const dateParts = {
+        Year: date.getFullYear(),
+        month: date.getMonth(),
+        date: date.getDate()
+    }
+
+    return `${dateParts.Year}/${dateParts.month + 1}/${dateParts.date}`
+}
